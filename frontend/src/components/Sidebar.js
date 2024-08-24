fb60c299bb960dfe34a5823b5e7779de0330f63b
Fred <fredtodd101@gmail.com>
2024-08-24 22:50:23 +0100

    added a climb counter to the side bar on the homepage and else where

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,12 +1,33 @@
-import React, { useContext } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import AuthContext from '../contexts/AuthContext';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
+import axios from 'axios';
 import './Sidebar.css'; // Import the CSS file
 
 const Sidebar = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
+  const [error, setError] = useState('');
+  const [totalClimbs, setTotalClimbs] = useState(0);
+  const [username, setUsername] = useState('');
+  const [bio, setBio] = useState('');
+  const [profilePicture, setProfilePicture] = useState('');
+
+  const refreshToken = async () => {
+    try {
+      const oldToken = localStorage.getItem('token');
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/refresh`, {}, {
+        headers: { Authorization: `Bearer ${oldToken}` }
+      });
+      const { token } = response.data;
+      localStorage.setItem('token', token);
+      return token;
+    } catch (error) {
+      console.error('Error refreshing token:', error);
+      return null;
+    }
+  };
 
   const handleLoginClick = () => {
     navigate('/login');
@@ -17,6 +38,63 @@ const Sidebar = () => {
   };
 
   const hideAuthOptions = location.pathname === '/login' || location.pathname === '/register';
+
+  const fetchProfile = useCallback(async () => {
+    try {
+      let token = localStorage.getItem('token');
+      if (!token) {
+        setError('No token found. Please log in.');
+        return;
+      }
+
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const response = await axios.get(`${apiUrl}/auth/profile`, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      const { username, bio, profile_picture } = response.data;
+      setUsername(username);
+      setBio(bio);
+      setProfilePicture(profile_picture);
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        const newToken = await refreshToken();
+        if (newToken) {
+          fetchProfile();
+        } else {
+          setError('Session expired. Please log in again.');
+        }
+      } else {
+        console.error('Error fetching profile:', error);
+        setError('Error fetching profile data');
+      }
+    }
+  }, []);
+
+  const fetchTotalClimbs = useCallback(async () => {
+    try {
+      let token = localStorage.getItem('token');
+      if (!token) {
+        setError('No token found. Please log in.');
+        return;
+      }
+      const apiUrl = `${process.env.REACT_APP_API_URL}/auth/climbs`;
+      const response = await axios.get(apiUrl, {
+        headers: { Authorization: `Bearer ${token}` }
+      });
+
+      setTotalClimbs(response.data.length);
+    } catch (error) {
+      console.error('Error fetching total climbs:', error);
+      setError('Error fetching total climbs');
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchProfile();
+    fetchTotalClimbs();
+  }, [fetchProfile, fetchTotalClimbs]);
+
 
   return (
     <div className="sidebar">
@@ -39,7 +117,8 @@ const Sidebar = () => {
         )}
       </div>
       <div className="feed-section">
-        <p>Your Feed</p>
+          <div className="climb-number">{totalClimbs}</div>
+          <div className="climb-text">Boulders</div>
         <p>Global Leaderboard</p>
       </div>
     </div>
